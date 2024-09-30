58a3307ba6e79b0aeba71bf467eb9a970914685d
Ajala Matthew Adekunle <80929400+SymplyMatt@users.noreply.github.com>
2024-09-30 12:07:53 +0100

    removed comments

--- src/models/associations.ts
+++ src/models/associations.ts
@@ -1,8 +1,6 @@
-// src/models/associations.ts
 import Subject from './Subject';
 import Topic from './Topic';
 
-// Define the associations here
 Subject.hasMany(Topic, {
   foreignKey: 'subject',
   as: 'topics',

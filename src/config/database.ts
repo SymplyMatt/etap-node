0c83cac41105c5dbecff6ad0612077000c1fe7a8
Ajala Matthew Adekunle <80929400+SymplyMatt@users.noreply.github.com>
2024-10-02 03:09:47 +0100

    added port

--- src/config/database.ts
+++ src/config/database.ts
@@ -9,6 +9,7 @@ export const sequelize = new Sequelize(
   process.env.DB_PASSWORD || 'your_postgres_password',
   {
     host: process.env.DB_HOST || '127.0.0.1',
+    port: 5433,
     dialect: 'postgres',
   }
 );

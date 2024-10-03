5de5ca4c9bb810625e2689a4e5cde435972a6cf7
Ajala Matthew Adekunle <80929400+SymplyMatt@users.noreply.github.com>
2024-10-04 00:40:01 +0100

    allowed origin

--- src/config/allowedOrigins.ts
+++ src/config/allowedOrigins.ts
@@ -4,6 +4,8 @@ const allowedOrigins : String[]= [
    "http://localhost:1234",
    "https://e-tap-46ce93b99242.herokuapp.com/",
    "https://e-tap-46ce93b99242.herokuapp.com",
+   "https://e-tap-test.vercel.app",
+   "https://e-tap-test.vercel.app/",
  ];
  
  export default allowedOrigins;

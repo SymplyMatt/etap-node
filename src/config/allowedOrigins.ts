7e2a72c3dc0b97565538a1926af46f8fa770250d
Ajala Matthew Adekunle <80929400+SymplyMatt@users.noreply.github.com>
2024-10-04 00:04:32 +0100

    allowed origins

--- src/config/allowedOrigins.ts
+++ src/config/allowedOrigins.ts
@@ -2,16 +2,8 @@ const allowedOrigins : String[]= [
    "http://localhost:3000",
    "http://localhost:5173",
    "http://localhost:1234",
-   "https://test-api.rhmstech.com/",
-   "https://test-api.rhmstech.com",
-   "https://test-web.rhmstech.com/",
-   "https://test-web.rhmstech.com",
-   "http://192.168.0.144:3000/",
-   "http://192.168.0.144:3000",
-   "http://192.168.0.3:3000/",
-   "http://192.168.0.3:3000",
-   "http://127.0.0.1:5500",
-   "http://127.0.0.1:5500/",
+   "https://e-tap-46ce93b99242.herokuapp.com/",
+   "https://e-tap-46ce93b99242.herokuapp.com",
  ];
  
  export default allowedOrigins;
